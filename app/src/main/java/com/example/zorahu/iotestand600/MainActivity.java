aa479eddb6bf212adc80681b0ae73e28f7939c44
DawnOfHope <DawnOfHope.zora@gmail.com>
2016-09-19 14:12:41 +0800

    Handle the permissions request response is ok

--- app/src/main/java/com/example/zorahu/iotestand600/MainActivity.java
+++ app/src/main/java/com/example/zorahu/iotestand600/MainActivity.java
@@ -2,9 +2,12 @@ package com.example.zorahu.iotestand600;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,7 +23,24 @@ public class MainActivity extends AppCompatActivity {
                 == PackageManager.PERMISSION_GRANTED;
 
         if (!hasPermission){
+            ActivityCompat.requestPermissions(this,
+                    new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
+                    1);
+        }
+    }
+    //Handle the permissions request response
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permission,@NonNull int[] grantResult){
+        super.onRequestPermissionsResult(requestCode,permission,grantResult);
+        if (requestCode == 1){
 
+            if (grantResult.length > 0
+                    && grantResult[0] == PackageManager.PERMISSION_GRANTED){
+
+                Log.d("test","OK");
+            }else {
+                Log.d("test","不Ok");
+            }
         }
     }
 }
